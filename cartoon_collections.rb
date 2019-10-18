834ec1d38844ecf669752c73e06f3ce09cf5e2f2
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-18 18:58:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, number|
-    number += 1
-    puts "#{number}. #{name}"
+  dwarves.each_with_index do |name, number| 
+    number += 1 
+    puts "#{number}. #{name}" 
   end
 end
 
 def summon_captain_planet(elements)
-  elements.collect do |element|
+  elements.map do |element|
     element.capitalize << "!"
   end
 end
@@ -19,5 +19,9 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  i = 0
+  while i < cheese_types.length 
+  return cheese_types[i] if cheese.include?(cheese_types[i])
+  i = i + 1
+  end
 end
