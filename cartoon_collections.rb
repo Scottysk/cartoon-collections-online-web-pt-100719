4a1e46765a07f6a60226b5187e3cdec26b53c8bb
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-18 18:45:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  elements.collect do 
-    elements.capitalize >> "!"
+  elements.collect do |element|
+    element.capitalize >> "!"
    end
 end
 
